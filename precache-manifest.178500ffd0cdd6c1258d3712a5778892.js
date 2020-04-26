d61743a524e76bd076cb8758594db776715b6cdf
“ilroman” <il.roman@ukr.net>
2020-04-26 13:05:17 +0300

    Updates

---
+++ precache-manifest.178500ffd0cdd6c1258d3712a5778892.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a4bd2506a90a3c76bddd16652aadf20",
+    "url": "/IlRoman/TodoList-react_redux/index.html"
+  },
+  {
+    "revision": "d3c9af9c706b8f95c601",
+    "url": "/IlRoman/TodoList-react_redux/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "84898f255d2fbddf8920",
+    "url": "/IlRoman/TodoList-react_redux/static/js/2.c6e1514d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IlRoman/TodoList-react_redux/static/js/2.c6e1514d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c9af9c706b8f95c601",
+    "url": "/IlRoman/TodoList-react_redux/static/js/main.bb82685d.chunk.js"
+  },
+  {
+    "revision": "8916f612ab66477cfe92",
+    "url": "/IlRoman/TodoList-react_redux/static/js/runtime-main.8b9d1591.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/IlRoman/TodoList-react_redux/static/media/logo.5d5d9eef.svg"
+  }
+]);
